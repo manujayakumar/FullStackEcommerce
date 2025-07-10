ec87adf3c21232810cd580d4c6a5d11b24a80b24
Manu Jayakumar <99114381+manujayakumar@users.noreply.github.com>
2025-07-10 10:01:23 +0530

    setup api and express

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,22 +1,14 @@
 import express from "express";
-import productRoute from "./routes/productRoute";
-import productRouter from "./routes/products"
-
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 // middlewares
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // routes
-app.get('/', (req, res) => {
-    res.status(201).send("Hi, there..");
-});
-app.use('/reminder', productRoute);
 
-app.use('/products', productRouter);
 // error handlers
 
 app.listen(PORT, () => console.log(`The server is successfully running on PORT: ${PORT}`));
